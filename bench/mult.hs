e930e7482950893b20f4dda13e66e78be7dfb800
Daniel Diaz <dhelta.diaz@gmail.com>
2013-03-21 13:16:28 -0400

    Added benchmarks.

---
+++ bench/mult.hs
@@ -0,0 +1,25 @@
+
+import Criterion.Main
+--
+import Data.Matrix
+
+mat :: Int -> Matrix Int
+mat n = matrix n n $ \(i,j) -> i - j
+
+testdef :: Int -> Matrix Int
+testdef n = multStd (mat n) (mat n)
+
+teststr :: Int -> Matrix Int
+teststr n = multStrassen (mat n) (mat n)
+
+teststrm :: Int -> Matrix Int
+teststrm n = multStrassenMixed (mat n) (mat n)
+
+bmat :: Int -> Benchmark
+bmat n = bgroup ("mult" ++ show n)
+ [ bench "Definition" $ nf testdef n
+ , bench "Strassen mixed" $ nf teststrm n
+ ]
+
+main :: IO ()
+main = defaultMain $ fmap bmat [8,10,20,100,250,500,1000]
