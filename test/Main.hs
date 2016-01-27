c8fdde11fbf3aec7e10837673152e54eaef90d60
octopuscabbage <octopuscabbage@gmail.com>
2016-01-26 20:39:30 -0600

    reverted test changes to be behind pr #21

--- test/Main.hs
+++ test/Main.hs
@@ -2,13 +2,11 @@
 import Data.Matrix
 import Data.Ratio
 import Control.Applicative
-import Data.Monoid
+import Data.Monoid (mconcat)
 
 import Test.Tasty
 import qualified Test.Tasty.QuickCheck as QC
 import Test.QuickCheck
-import Test.QuickCheck.Function
-import Test.Hspec
 
 {- matrix package test set
 
@@ -38,9 +36,6 @@ instance Arbitrary a => Arbitrary (Matrix a) where
     I m <- arbitrary
     genMatrix' n m
 
-instance Arbitrary a => Arbitrary (Sum a) where
-  arbitrary = Sum <$> arbitrary
-
 genMatrix' :: Arbitrary a => Int -> Int -> Gen (Matrix a)
 genMatrix' n m = fromList n m <$> vector (n*m)
 
@@ -59,91 +54,75 @@ instance Arbitrary Sq where
     Sq <$> genMatrix n n
 
 main :: IO ()
-main = hspec $ parallel $ describe "matrix tests" $do 
-    it "zero n m = matrix n m (const 0)"$ property
+main = defaultMain $ testGroup "matrix tests" [
+    QC.testProperty "zero n m = matrix n m (const 0)"
        $ \(I n) (I m) -> zero n m == matrix n m (const 0)
-    it "identity * m = m * identity = m" $ property
+  , QC.testProperty "identity * m = m * identity = m"
        $ \(Sq m) -> let n = nrows m in identity n * m == m && m * identity n == m
-    it "a * (b * c) = (a * b) * c" $ property
+  , QC.testProperty "a * (b * c) = (a * b) * c"
        $ \(I a) (I b) (I c) (I d) -> forAll (genMatrix a b)
        $ \m1 -> forAll (genMatrix b c)
        $ \m2 -> forAll (genMatrix c d)
        $ \m3 -> m1 * (m2 * m3) == (m1 * m2) * m3
-    it "multStd a b = multStd2 a b" $ property
+  , QC.testProperty "multStd a b = multStd2 a b"
        $ \(I a) (I b) (I c) -> forAll (genMatrix a b)
        $ \m1 -> forAll (genMatrix b c)
        $ \m2 -> multStd m1 m2 == multStd2 m1 m2
-    it "getMatrixAsVector m = mconcat [ getRow i m | i <- [1 .. nrows m]]" $ property
+  , QC.testProperty "getMatrixAsVector m = mconcat [ getRow i m | i <- [1 .. nrows m]]"
        $ \m -> getMatrixAsVector (m :: Matrix R) == mconcat [ getRow i m | i <- [1 .. nrows m] ]
-    it  "fmap id = id" $ property
+  , QC.testProperty "fmap id = id"
        $ \m -> fmap id m == (m :: Matrix R)
-    it "permMatrix n i j * permMatrix n i j = identity n" $ property 
+  , QC.testProperty "permMatrix n i j * permMatrix n i j = identity n"
        $ \(I n) -> forAll (choose (1,n))
        $ \i     -> forAll (choose (1,n))
        $ \j     -> permMatrix n i j * permMatrix n i j == identity n
-    it  "setElem (getElem i j m) (i,j) m = m" $ property
+  , QC.testProperty "setElem (getElem i j m) (i,j) m = m"
        $ \m -> forAll (choose (1,nrows m))
        $ \i -> forAll (choose (1,ncols m))
        $ \j -> setElem (getElem i j m) (i,j) m == (m :: Matrix R)
-    it "transpose (transpose m) = m" $ property 
+  , QC.testProperty "transpose (transpose m) = m"
        $ \m -> transpose (transpose m) == (m :: Matrix R)
-    it "if m' = setSize e r c m then (nrows m' = r) && (ncols m' = c)" $ property
+  , QC.testProperty "if m' = setSize e r c m then (nrows m' = r) && (ncols m' = c)"
        $ \e (I r) (I c) m -> let m' :: Matrix R ; m' = setSize e r c m in nrows m' == r && ncols m' == c
-    it "if (nrows m = r) && (nrcols m = c) then setSize _ r c m = m" $ property
+  , QC.testProperty "if (nrows m = r) && (nrcols m = c) then setSize _ r c m = m"
        $ \m -> let r = nrows m
                    c = ncols m
                in  setSize undefined r c m == (m :: Matrix R)
-    it "getRow i m = getCol i (transpose m)" $ property
+  , QC.testProperty "getRow i m = getCol i (transpose m)"
        $ \m -> forAll (choose (1,nrows m))
        $ \i -> getRow i (m :: Matrix R) == getCol i (transpose m)
-    it "joinBlocks (splitBlocks i j m) = m" $ property
+  , QC.testProperty "joinBlocks (splitBlocks i j m) = m"
        $ \m -> forAll (choose (1,nrows m))
        $ \i -> forAll (choose (1,ncols m))
        $ \j -> joinBlocks (splitBlocks i j m) == (m :: Matrix R)
-    it "scaleMatrix k m = fmap (*k) m" $ property
+  , QC.testProperty "scaleMatrix k m = fmap (*k) m"
        $ \k m -> scaleMatrix k m == fmap (*k) (m :: Matrix R)
-    it "(+) = elementwise (+)" $ property
+  , QC.testProperty "(+) = elementwise (+)"
        $ \m1 -> forAll (genMatrix (nrows m1) (ncols m1))
        $ \m2 -> m1 + m2 == elementwise (+) m1 m2
-    it"switchCols i j = transpose . switchRows i j . transpose" $ property
+  , QC.testProperty "switchCols i j = transpose . switchRows i j . transpose"
        $ \m -> forAll (choose (1,ncols m))
        $ \i -> forAll (choose (1,ncols m))
        $ \j -> switchCols i j (m :: Matrix R) == (transpose $ switchRows i j $ transpose m)
-    it"detLaplace (fromList 3 3 $ repeat 1) = 0"$ property
+  , QC.testProperty "detLaplace (fromList 3 3 $ repeat 1) = 0"
        $ detLaplace (fromList 3 3 $ repeat 1) == 0
-    it "if (u,l,p,d) = luDecomp m then (p*m = l*u) && (detLaplace p = d)" $ property
+  , QC.testProperty "if (u,l,p,d) = luDecomp m then (p*m = l*u) && (detLaplace p = d)"
        $ \(Sq m) -> (detLaplace m /= 0) ==>
              (let (u,l,p,d) = luDecompUnsafe m in p*m == l*u && detLaplace p == d)
-    it "detLaplace m = detLU m" $ property
+  , QC.testProperty "detLaplace m = detLU m"
        $ \(Sq m) -> detLaplace m == detLU m
-    it "if (u,l,p,q,d,e) = luDecomp' m then (p*m*q = l*u) && (detLU p = d) && (detLU q = e)" $ property
+  , QC.testProperty "if (u,l,p,q,d,e) = luDecomp' m then (p*m*q = l*u) && (detLU p = d) && (detLU q = e)"
        $ \(Sq m) -> (detLU m /= 0) ==>
              (let (u,l,p,q,d,e) = luDecompUnsafe' m in p*m*q == l*u && detLU p == d && detLU q == e)
-    it "detLU (scaleRow k i m) = k * detLU m" $ property
+  , QC.testProperty "detLU (scaleRow k i m) = k * detLU m"
        $ \(Sq m) k -> forAll (choose (1,nrows m))
        $ \i -> detLU (scaleRow k i m) == k * detLU m
-    it "let n = nrows m in detLU (switchRows i j m) = detLU (permMatrix n i j) * detLU m"  $ property
+  , QC.testProperty "let n = nrows m in detLU (switchRows i j m) = detLU (permMatrix n i j) * detLU m"
        $ \(Sq m) -> let n = nrows m in forAll (choose (1,n))
        $ \i      -> forAll (choose (1,n))
        $ \j      -> detLU (switchRows i j m) == detLU (permMatrix n i j) * detLU m
-    it  "fromList n m . toList = id" $ property
+  , QC.testProperty "fromList n m . toList = id"
        $ \m -> fromList (nrows m) (ncols m) (toList m) == (m :: Matrix R)
-    it "fromLists . toLists = id" $ property 
-       $ \m -> fromLists (toLists m) == (m :: Matrix (Sum Int))
-    it "monoid law: mappend mempty x = x" $ property
-       $ \x -> mappend mempty (x :: Matrix (Sum Int)) == x
-    it "monoid law: mappend x mempty = x" $ property
-       $ \x -> mappend (x :: Matrix (Sum Int)) mempty == x
-    it "monoid law: mappend x (mappend y z) = mappend (mappend x y) z " $ property
-       $ \x y z -> mappend (x :: Matrix (Sum Int)) (mappend (y::Matrix (Sum Int)) (z::Matrix (Sum Int))) == mappend (mappend x y) z
-    it "applicative law - identity: pure id <*> v = v" $ property
-       $ \x -> (pure id <*> (x :: (Matrix Int))) == x
-    {-- I couldn't get this one towork
-    it "applicative law - composition: pure (.) <*> u <*> v <*> w = u <*> (v <*> w)" $ property
-       $ \u v w -> (pure (.) <*> (u :: Matrix (Int->Int)) <*> (v::Matrix(Int->Int)) <*> (w::Matrix(Int->Int))) == (u <*> (v <*> w))
-    it "applicative law - homomorphism: pure f <*> pure x = pure (f x)" $ property
-       $ \(Fun _ f) x -> (pure f <*> pure (x :: Int)) == pure (f x)
-    it "applicative law - interchange: u <*> pure y = pure ($ y) <*> u" $ property
-       $ \(Fun _ u) y -> (u <*> pure (y :: Int)) == (pure ($ y ) <*> u)
-    --}
-
+  , QC.testProperty "fromLists . toLists = id"
+       $ \m -> fromLists (toLists m) == (m :: Matrix R)
+    ]
