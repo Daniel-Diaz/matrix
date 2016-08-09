3fed4a7d3d2a6b7c3026e64f28625954332bf9b9
Andrew Cowie <andrew@operationaldynamics.com>
2016-08-09 23:25:20 +1000

    Preliminary, naive use of box drawing characters

--- Data/Matrix.hs
+++ Data/Matrix.hs
@@ -129,8 +129,11 @@ sizeStr n m = show n ++ "x" ++ show m
 
 -- | Display a matrix as a 'String' using the 'Show' instance of its elements.
 prettyMatrix :: Show a => Matrix a -> String
-prettyMatrix m@(M _ _ _ _ _ v) = unlines
- [ "( " <> unwords (fmap (\j -> fill mx $ show $ m ! (i,j)) [1..ncols m]) <> " )" | i <- [1..nrows m] ]
+prettyMatrix m@(M _ _ _ _ _ v) = concat
+  [ "┌ ", unwords (replicate (ncols m) (fill mx "")), " ┐\n"
+  , unlines [ "│ " <> unwords (fmap (\j -> fill mx $ show $ m ! (i,j)) [1..ncols m]) <> " │" | i <- [1..nrows m] ]
+  , "└ ", unwords (replicate (ncols m) (fill mx "")), " ┘\n"
+  ]
  where
   mx = V.maximum $ fmap (length . show) v
   fill k str = replicate (k - length str) ' ' ++ str
